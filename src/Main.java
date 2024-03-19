35f4efac6955d321bfed76542a9057d11a1280ae
Adam <AdamGitowy@gmail.com>
2024-03-19 12:00:26 +0100

    Added fields, a constructor and getter methods to class Book

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,25 @@ class LibraryManagementSystem {
 }
 
 class Book {
+    //Fields declarations
+    private String title;
+    private String author;
+    private String ISBN;
 
+    //constructor
+
+    public Book(String title, String author, String ISBN) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+    }
+
+    //getter methods
+    public String getTitle() {return title;}
+
+    public String getAuthor() {return author;}
+
+    public String getISBN() {return ISBN;}
 }
 
 class User {
