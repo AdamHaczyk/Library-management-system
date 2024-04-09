aa1488330b269c0ab24217fb70a349f8a3e82eb7
Adam <AdamGitowy@gmail.com>
2024-04-09 12:02:19 +0200

    Added comments, got rid of some warnings, replaced long strings of text with text blocks to make it look cleaner

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        int optionSelection = 0;
+        int optionSelection;
 
-        //LibraryManagementSystem library1 = new LibraryManagementSystem();     WORK IN PROGRESS
-
-        System.out.println ("1 = manage books\n" +
-                            "2 = manage user account\n" +
-                            "3 = idk");
+        //Basic interface for user interaction. In future this will
+        //be the first step of interacting with the whole program
+        System.out.println ("""
+                    1 = manage books\n
+                    2 = manage user account\n
+                    3 = idk""");
 
         Scanner scanner = new Scanner(System.in);
         optionSelection = scanner.nextInt();
-        //scanner.close();
 
+        //Switch statement will be expanded later
         switch (optionSelection) {
             case 1:
                 bookManagement();
                 break;
         }
 
+        scanner.close();
     }
 
+    //Below method allows interaction between classes Main and Book
     public static void bookManagement() throws SQLException {
 
-        int optionSelection = 0;
+        int optionSelection;
 
         String url = "jdbc:mysql://localhost:3306/books";
         String username = "adam";
@@ -40,13 +43,18 @@ public class Main {
         String title;
         String ISBN;
 
+        //Connection set up (object connection is passed on as an argument
+        //methods of the Book class) and a basic interface to allow
+        //selection of options provided by the Book class
         Connection connection = DriverManager.getConnection(url, username, password);
-        System.out.println( "1 = Check in\n" +
-                            "2 = Check out\n" +
-                            "3 = Browse");
+        System.out.println("""
+                1 = Check in
+                2 = Check out
+                3 = Browse""");
         Scanner scanner = new Scanner(System.in);
         optionSelection = scanner.nextInt();
 
+        //Switch statement to be expanded with more options later
         switch (optionSelection) {
             case 2:
                 Book browserObject = new Book();
@@ -68,6 +76,7 @@ public class Main {
     }
 }
 
+//LibraryManagementSystem has no functionality as of yet
 class LibraryManagementSystem {
 
     public LibraryManagementSystem() {
@@ -110,7 +119,7 @@ class Book {
     private String author;
     private String ISBN;
 
-    //constructors
+    //Constructors
     public Book() {/*empty constructor*/}
 
     public Book(int book_id) {
@@ -125,52 +134,54 @@ class Book {
         this.author = author;
         this.title = title;
     }
-    public Book(int book_id, String title, String author, String ISBN) throws SQLException {
+    public Book(int book_id, String title, String author, String ISBN) {
         this.book_id = book_id;
         this.title = title;
         this.author = author;
         this.ISBN = ISBN;
     }
 
-    //getter methods
+    //Getter methods
     public int getBook_id() {return book_id;}
     public String getTitle() {return title;}
-
     public String getAuthor() {return author;}
-
     public String getISBN() {return ISBN;}
 
 
-    public void browseBooks(Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM books;");
-        int book_id;
-        String author;
-        String title;
-        String ISBN;
-        boolean isAvailable;
+    public void browseBooks(Connection connection) {
 
-        System.out.println("Book database:");
-        while(resultSet.next()) {
-            book_id = resultSet.getInt("book_id");
-            author = resultSet.getString("author");
-            title = resultSet.getString("title");
-            ISBN = resultSet.getString("ISBN");
-            isAvailable = resultSet.getBoolean("is_available");
+        try(Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM books;");
+            int book_id;
+            String author;
+            String title;
+            String ISBN;
+            boolean isAvailable;
 
-            System.out.print("book id: " + book_id + "  ||  ");
-            System.out.print("author: " + author + "  ||  ");
-            System.out.print("title: " + title + "  ||  ");
-            System.out.print("ISBN: " + ISBN + "  ||  ");
-            System.out.println("Available: " + isAvailable);
+            System.out.println("Book database:");
+            while (resultSet.next()) {
+                book_id = resultSet.getInt("book_id");
+                author = resultSet.getString("author");
+                title = resultSet.getString("title");
+                ISBN = resultSet.getString("ISBN");
+                isAvailable = resultSet.getBoolean("is_available");
 
+                System.out.print("book id: " + book_id + "  ||  ");
+                System.out.print("author: " + author + "  ||  ");
+                System.out.print("title: " + title + "  ||  ");
+                System.out.print("ISBN: " + ISBN + "  ||  ");
+                System.out.println("Available: " + isAvailable);
+
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
 
     }
     public void checkOutBook(Connection connection) throws SQLException {
 
         connection.setAutoCommit(false);
-        try (Statement statement = connection.createStatement();) {
+        try (Statement statement = connection.createStatement()) {
             String query =
                     "UPDATE books " +
                             "SET is_available = FALSE " +
